791906a8468a1cf95a17314cb78259b6454d9963
youssef frahat <108248554+youssef-frahat@users.noreply.github.com>
2024-11-02 04:30:22 +0200

    make an onboarding

--- lib/splash/onbording.dart
+++ lib/splash/onbording.dart
@@ -18,23 +18,70 @@ class Boarding extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          CustomButton(
-              child: Text(
-                "login",
-                style: TextStyle(color: Colors.black),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.black.withOpacity(0.6),
+                Colors.black.withOpacity(0.3),
+              ],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+          ),
+          child: Column(
+            children: [
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  children: [
+                    const Text(
+                      "mosha store",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    const Text(
+                      "welcome to our store",
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                  ],
+                ),
               ),
-              onPressed: () => navigatepush(const Loginscrean(), context)),
-          const SizedBox(height: 20),
-          CustomButton(
-              child: Text(
-                "signup",
-                style: TextStyle(color: Colors.black),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  children: [
+                    CustomButton(
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () =>
+                          navigatepush(const Loginscrean(), context),
+                    ),
+                    const SizedBox(height: 20),
+                    CustomButton(
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        navigatepush(const Placeholder(), context);
+                      },
+                    ),
+                    const SizedBox(height: 50),
+                  ],
+                ),
               ),
-              onPressed: () {
-                navigatepush(const Placeholder(), context);
-              }),
-        ]),
+            ],
+          ),
+        ),
       ),
     );
   }
